d5271d2daa7c5fb67eab07ce248c515efa79b4be
David Clarke <terrorhawks@gmail.com>
2014-12-22 15:45:20 +0000

    change from service to resource

---
+++ app/resources/offer.js
@@ -0,0 +1,21 @@
+angular.module('common.resources', [])
+
+.factory('Offer', ['$resource', 'domainName', function ($resource, domainName) {
+
+  return  $resource(domainName + '/api/offers/:id', { id: '@id' }, {
+
+    create: {
+      method: 'POST'
+    },
+
+    update: {
+      method: 'PUT'
+    },
+    
+    query: {
+      isArray: false 
+    }
+
+  });
+
+}]);
