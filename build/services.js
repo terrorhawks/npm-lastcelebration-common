e9b90b4194274e59226426af17dabdfc903b4003
David Clarke <terrorhawks@gmail.com>
2015-01-05 14:32:38 +0000

    more debugging

--- build/services.js
+++ build/services.js
@@ -211,119 +211,150 @@ angular.module('common.services')
   var s3_config;
   var purge_date;
 
-  /*
-Copyright (c) 2011, Daniel Guerrero
-All rights reserved.
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL DANIEL GUERRERO BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+//   /*
+// Copyright (c) 2011, Daniel Guerrero
+// All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL DANIEL GUERRERO BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  */
 
-  /**
-   * Uses the new array typed in javascript to binary base64 encode/decode
-   * at the moment just decodes a binary base64 encoded
-   * into either an ArrayBuffer (decodeArrayBuffer)
-   * or into an Uint8Array (decode)
-   * 
-   * References:
-   * https://developer.mozilla.org/en/JavaScript_typed_arrays/ArrayBuffer
-   * https://developer.mozilla.org/en/JavaScript_typed_arrays/Uint8Array
-   */
-  var Base64Binary = {
-    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+//   *
+//    * Uses the new array typed in javascript to binary base64 encode/decode
+//    * at the moment just decodes a binary base64 encoded
+//    * into either an ArrayBuffer (decodeArrayBuffer)
+//    * or into an Uint8Array (decode)
+//    * 
+//    * References:
+//    * https://developer.mozilla.org/en/JavaScript_typed_arrays/ArrayBuffer
+//    * https://developer.mozilla.org/en/JavaScript_typed_arrays/Uint8Array
+   
+//   var Base64Binary = {
+//     _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
     
-    /* will return a  Uint8Array type */
-    decodeArrayBuffer: function(input) {
-      var bytes = (input.length/4) * 3;
-      var ab = new ArrayBuffer(bytes);
-      this.decode(input, ab);
+//     /* will return a  Uint8Array type */
+//     decodeArrayBuffer: function(input) {
+//       var bytes = (input.length/4) * 3;
+//       var ab = new ArrayBuffer(bytes);
+//       this.decode(input, ab);
       
-      return ab;
-    },
+//       return ab;
+//     },
     
-    decode: function(input, arrayBuffer) {
-      //get last chars to see if are valid
-      var lkey1 = this._keyStr.indexOf(input.charAt(input.length-1));    
-      var lkey2 = this._keyStr.indexOf(input.charAt(input.length-2));    
+//     decode: function(input, arrayBuffer) {
+//       //get last chars to see if are valid
+//       var lkey1 = this._keyStr.indexOf(input.charAt(input.length-1));    
+//       var lkey2 = this._keyStr.indexOf(input.charAt(input.length-2));    
     
-      var bytes = (input.length/4) * 3;
-      if (lkey1 == 64) bytes--; //padding chars, so skip
-      if (lkey2 == 64) bytes--; //padding chars, so skip
+//       var bytes = (input.length/4) * 3;
+//       if (lkey1 == 64) bytes--; //padding chars, so skip
+//       if (lkey2 == 64) bytes--; //padding chars, so skip
       
-      var uarray;
-      var chr1, chr2, chr3;
-      var enc1, enc2, enc3, enc4;
-      var i = 0;
-      var j = 0;
+//       var uarray;
+//       var chr1, chr2, chr3;
+//       var enc1, enc2, enc3, enc4;
+//       var i = 0;
+//       var j = 0;
       
-      if (arrayBuffer)
-        uarray = new Uint8Array(arrayBuffer);
-      else
-        uarray = new Uint8Array(bytes);
+//       if (arrayBuffer)
+//         uarray = new Uint8Array(arrayBuffer);
+//       else
+//         uarray = new Uint8Array(bytes);
       
-      input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+//       input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
       
-      for (i=0; i<bytes; i+=3) {  
-        //get the 3 octects in 4 ascii chars
-        enc1 = this._keyStr.indexOf(input.charAt(j++));
-        enc2 = this._keyStr.indexOf(input.charAt(j++));
-        enc3 = this._keyStr.indexOf(input.charAt(j++));
-        enc4 = this._keyStr.indexOf(input.charAt(j++));
+//       for (i=0; i<bytes; i+=3) {  
+//         //get the 3 octects in 4 ascii chars
+//         enc1 = this._keyStr.indexOf(input.charAt(j++));
+//         enc2 = this._keyStr.indexOf(input.charAt(j++));
+//         enc3 = this._keyStr.indexOf(input.charAt(j++));
+//         enc4 = this._keyStr.indexOf(input.charAt(j++));
     
-        chr1 = (enc1 << 2) | (enc2 >> 4);
-        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-        chr3 = ((enc3 & 3) << 6) | enc4;
+//         chr1 = (enc1 << 2) | (enc2 >> 4);
+//         chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+//         chr3 = ((enc3 & 3) << 6) | enc4;
     
-        uarray[i] = chr1;     
-        if (enc3 != 64) uarray[i+1] = chr2;
-        if (enc4 != 64) uarray[i+2] = chr3;
-      }
+//         uarray[i] = chr1;     
+//         if (enc3 != 64) uarray[i+1] = chr2;
+//         if (enc4 != 64) uarray[i+2] = chr3;
+//       }
     
-      return uarray;  
-    }
-  };
+//       return uarray;  
+//     }
+//   };
 
   //cached for 50 seconds
   var ttl_in_ms = 50000;
 
-  function postFormData(uri, formData) {
-    var deferred = $q.defer();
-      $http.post(uri, formData, {
-        transformRequest: angular.identity,
-        headers: {'Content-Type': undefined}
-      }).success(function (response, status) {
-        deferred.resolve(response);
-      }).error(function (error, status) {
-        deferred.reject(error);
-      });
-    return deferred.promise;
-  }
+  // function postFormData(uri, formData) {
+  //   var deferred = $q.defer();
+  //     $http.post(uri, formData, {
+  //       transformRequest: angular.identity,
+  //       headers: {'Content-Type': undefined}
+  //     }).success(function (response, status) {
+  //       deferred.resolve(response);
+  //     }).error(function (error, status) {
+  //       deferred.reject(error);
+  //     });
+  //   return deferred.promise;
+  // }
 
-  function createFormData(key, options, byteArray) {
-    var fd = new FormData();
-    fd.append('key', key + '.jpg');
-    fd.append('acl', 'public-read');
-    fd.append('Content-Type', 'image/jpeg');
-    fd.append('AWSAccessKeyId', options.key);
-    fd.append('policy', options.policy);
-    fd.append('signature', options.signature);
-    fd.append('file', byteArray);
-    return fd;
-  }
+  function uploadToS3(key, params, imageURI, s3URI) {
+ 
+        var deferred = $.Deferred(),
+            ft = new FileTransfer(),
+            options = new FileUploadOptions();
+ 
+        options.fileKey = "file";
+        options.fileName = fileName;
+        options.mimeType = "image/jpeg";
+        options.chunkedMode = false;
+        options.params = {
+            "key": key + '.jpg',
+            "AWSAccessKeyId": params.key,
+            "acl": 'public-read',
+            "policy": params.policy,
+            "signature": params.signature,
+            "Content-Type": "image/jpeg"
+        };
+ 
+        ft.upload(imageURI, s3URI,
+            function (e) {
+                deferred.resolve(e);
+            },
+            function (e) {
+                deferred.reject(e);
+            }, options);
+ 
+        return deferred.promise();
+ 
+    }
+
+  // function createFormData(key, options, byteArray) {
+  //   var fd = new FormData();
+  //   fd.append('key', key + '.jpg');
+  //   fd.append('acl', 'public-read');
+  //   fd.append('Content-Type', 'image/jpeg');
+  //   fd.append('AWSAccessKeyId', options.key);
+  //   fd.append('policy', options.policy);
+  //   fd.append('signature', options.signature);
+  //   fd.append('file', byteArray);
+  //   return fd;
+  // }
 
   function getAWSPolicy() {
     var deferred = $q.defer();
@@ -359,10 +390,12 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
         getAWSPolicy().then(function (options) {
           var file = folder + '/' + key;
           var file_uri = s3Uri + file;
-          console.log(media.data);
-          var byteArray = Base64Binary.decodeArrayBuffer(media.data);  
-          var fd = createFormData(file,  options, byteArray);
-          postFormData(s3Uri, fd).then(function (response) {
+
+          console.log(media.local_uri);
+          //var byteArray = Base64Binary.decodeArrayBuffer(media.data);  
+          uploadToS3(file, options, media.local_uri, file_uri).then(function (response) {
+          //var fd = createFormData(file,  options, byteArray);
+          //postFormData(s3Uri, fd).then(function (response) {
             media.content_type =  'image/jpg';
             media.uri = file_uri;
             deferred.resolve(media);
