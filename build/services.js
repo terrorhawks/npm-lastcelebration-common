04b5ad23f10b50ad33f78b0035e038956fa061ff
David Clarke <terrorhawks@gmail.com>
2015-01-06 11:24:51 +0000

    refactored API due to issues on iOS

--- build/services.js
+++ build/services.js
@@ -206,97 +206,10 @@ angular.module('common.services')
 }]);
 angular.module('common.services')
 
-.factory('S3', ['$q', '$http', 'domainName', 'awsImageUploadBucket', function($q, $http, domainName, awsImageUploadBucket) {
+.factory('S3', ['$q', '$http', '$cordovaDevice', 'domainName', 'awsImageUploadBucket', 'uuid4', function($q, $http, $cordovaDevice, domainName, awsImageUploadBucket, uuid4) {
 
   var s3_config;
   var purge_date;
-
-//   /*
-// Copyright (c) 2011, Daniel Guerrero
-// All rights reserved.
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL DANIEL GUERRERO BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//  */
-
-//   *
-//    * Uses the new array typed in javascript to binary base64 encode/decode
-//    * at the moment just decodes a binary base64 encoded
-//    * into either an ArrayBuffer (decodeArrayBuffer)
-//    * or into an Uint8Array (decode)
-//    * 
-//    * References:
-//    * https://developer.mozilla.org/en/JavaScript_typed_arrays/ArrayBuffer
-//    * https://developer.mozilla.org/en/JavaScript_typed_arrays/Uint8Array
-   
-//   var Base64Binary = {
-//     _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-    
-//     /* will return a  Uint8Array type */
-//     decodeArrayBuffer: function(input) {
-//       var bytes = (input.length/4) * 3;
-//       var ab = new ArrayBuffer(bytes);
-//       this.decode(input, ab);
-      
-//       return ab;
-//     },
-    
-//     decode: function(input, arrayBuffer) {
-//       //get last chars to see if are valid
-//       var lkey1 = this._keyStr.indexOf(input.charAt(input.length-1));    
-//       var lkey2 = this._keyStr.indexOf(input.charAt(input.length-2));    
-    
-//       var bytes = (input.length/4) * 3;
-//       if (lkey1 == 64) bytes--; //padding chars, so skip
-//       if (lkey2 == 64) bytes--; //padding chars, so skip
-      
-//       var uarray;
-//       var chr1, chr2, chr3;
-//       var enc1, enc2, enc3, enc4;
-//       var i = 0;
-//       var j = 0;
-      
-//       if (arrayBuffer)
-//         uarray = new Uint8Array(arrayBuffer);
-//       else
-//         uarray = new Uint8Array(bytes);
-      
-//       input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-      
-//       for (i=0; i<bytes; i+=3) {  
-//         //get the 3 octects in 4 ascii chars
-//         enc1 = this._keyStr.indexOf(input.charAt(j++));
-//         enc2 = this._keyStr.indexOf(input.charAt(j++));
-//         enc3 = this._keyStr.indexOf(input.charAt(j++));
-//         enc4 = this._keyStr.indexOf(input.charAt(j++));
-    
-//         chr1 = (enc1 << 2) | (enc2 >> 4);
-//         chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-//         chr3 = ((enc3 & 3) << 6) | enc4;
-    
-//         uarray[i] = chr1;     
-//         if (enc3 != 64) uarray[i+1] = chr2;
-//         if (enc4 != 64) uarray[i+2] = chr3;
-//       }
-    
-//       return uarray;  
-//     }
-//   };
-
   //cached for 50 seconds
   var ttl_in_ms = 50000;
 
@@ -335,7 +248,7 @@ angular.module('common.services')
             "signature": params.signature,
             "Content-Type": undefined
         };
-        console.log("uploading...");
+        console.log("uploading to s3...");
         console.log(imageURI);
         console.log(s3URI);
         console.log(options);
@@ -382,29 +295,19 @@ angular.module('common.services')
     return deferred.promise;
   }
 
-    return {
+  return {
     
     sha: function(email) {
       return sha1(email);
     },
 
-    upload: function(media, key, folder) {
-      console.log("UPLOAD FUNCTION CALL");
-      console.log(media);
-      console.log(key);
-      console.log(folder);
-      console.log("UPLOAD Function starting");
-      var image_uri = media.local_uri;
-      var deferred = $q.defer();
-      
-      if (media.uri) {
-        //already been uploaded
-        deferred.resolve(media);
-      } else {
-        var s3Uri = 'https://' + awsImageUploadBucket + '.s3.amazonaws.com/';      
-        getAWSPolicy().then(function (options) {
-          //var file = folder + '/' + key;
-          var file = key;
+    upload: function(image_uri) {
+      var deferred = $q.defer();        
+      getAWSPolicy().then(function (options) {
+          var s3Uri = 'https://' + awsImageUploadBucket + '.s3.amazonaws.com/';      
+          var folder = $cordovaDevice.getUUID();
+          //var file = folder + '/' + uuid4.generate();
+          var file = uuid4.generate();
           var file_uri = s3Uri + file;
 
           console.log("Upload image from " + image_uri);
@@ -412,16 +315,13 @@ angular.module('common.services')
           uploadToS3(file, options, image_uri, file_uri).then(function (response) {
           //var fd = createFormData(file,  options, byteArray);
           //postFormData(s3Uri, fd).then(function (response) {
-            media.content_type =  'image/jpg';
-            media.uri = file_uri;
-            deferred.resolve(media);
+            deferred.resolve(file_uri);
           }, function (error) {
             deferred.reject(error);
           });
         }, function(error) {
           deferred.reject(error);
         });
-      }
       return deferred.promise;
     }
   };
