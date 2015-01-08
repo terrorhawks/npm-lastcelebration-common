a38684ab5950abccdfaa50984067cacb2d48dfc0
David Clarke <terrorhawks@gmail.com>
2015-01-08 15:35:36 +0000

    Add site resource

--- build/resources.js
+++ build/resources.js
@@ -66,3 +66,24 @@ angular.module('common.resources')
   });
 
 }]);
+angular.module('common.resources')
+
+.factory('Site', ['$resource', 'domainName', function ($resource, domainName) {
+
+  return  $resource(domainName + '/api/sites/:id', { id: '@id' }, {
+
+    create: {
+      method: 'POST'
+    },
+
+    update: {
+      method: 'PUT'
+    },
+    
+    query: {
+      isArray: true
+    }
+
+  });
+
+}]);
