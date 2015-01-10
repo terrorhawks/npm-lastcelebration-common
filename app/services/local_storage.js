20cc40a52b88dfc1a6c827e6d6f4de08e63e37db
David Clarke <terrorhawks@gmail.com>
2015-01-10 11:28:23 +0000

    don't allow undefined to be stringified

--- app/services/local_storage.js
+++ app/services/local_storage.js
@@ -2,15 +2,23 @@ angular.module('common.services')
 
 .factory('$localstorage', ['$window', function($window) {
   return {
+    
     set: function(key, value) {
       $window.localStorage[key] = value;
     },
+
     get: function(key, defaultValue) {
       return $window.localStorage[key] || defaultValue;
     },
+
     setObject: function(key, value) {
-      $window.localStorage[key] = JSON.stringify(value);
+      if (value) {
+        $window.localStorage[key] = JSON.stringify(value);
+      } else {
+        $window.localStorage[key] = undefined;
+      }
     },
+
     getObject: function(key) {
       var cached_object = $window.localStorage[key];
       if (cached_object) {
