dd393deda90d1c9d92a22a9798d00fac26bd22f5
David Clarke <terrorhawks@gmail.com>
2015-01-29 11:31:43 +0000

    avoid change on keydown

--- build/lc-common.js
+++ build/lc-common.js
@@ -506,7 +506,7 @@ angular.module('common.directives')
         link: function($scope, $element, $attrs, ngModelCtrl) {
             var listener = function() {
                if ($element.val()) {
-                var value = $element.val().replace(/\s/g, '');
+                var value = $element.val().replace(/\s{2}/g, ' ');
                 $element.val($filter('postcode')(value, false));
               }
             };
@@ -514,7 +514,7 @@ angular.module('common.directives')
             // This runs when we update the text field
             ngModelCtrl.$parsers.push(function(viewValue) {
               if (viewValue) {
-                return viewValue.replace(/\s/g, '');
+                return viewValue.replace(/\s{2}/g, '');
               } else {
                 return viewValue;
               }
@@ -526,13 +526,13 @@ angular.module('common.directives')
             };
             
             $element.bind('change', listener);
-            $element.bind('keydown', function(event) {
-                var key = event.keyCode;
-                // If the keys include the CTRL, SHIFT, ALT, or META keys, or the arrow keys, do nothing.
-                // This lets us support copy and paste too
-                if (key == 91 || (15 < key && key < 19) || (37 <= key && key <= 40)) return; 
-                $browser.defer(listener); // Have to do this or changes don't get picked up properly
-            });
+            // $element.bind('keydown', function(event) {
+            //     var key = event.keyCode;
+            //     // If the keys include the CTRL, SHIFT, ALT, or META keys, or the arrow keys, do nothing.
+            //     // This lets us support copy and paste too
+            //     if (key == 91 || (15 < key && key < 19) || (37 <= key && key <= 40)) return; 
+            //     $browser.defer(listener); // Have to do this or changes don't get picked up properly
+            // });
             
             $element.bind('paste cut', function() {
                 $browser.defer(listener);
