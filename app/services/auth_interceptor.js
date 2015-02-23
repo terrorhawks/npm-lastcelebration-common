79f108358386405e75734a2c46366f0c9147c392
David Clarke <terrorhawks@gmail.com>
2015-02-23 12:06:49 +0000

    change logic

--- app/services/auth_interceptor.js
+++ app/services/auth_interceptor.js
@@ -4,14 +4,14 @@ angular.module('common.services')
   return {
     request: function (config) {
       var not_aws_request = config.url.search(/s3.amazonaws.com/)===-1;
-      var clover_request = config.url.search(/api.eu.clover.com/)===-1;
+      var not_clover_request = config.url.search(/api.eu.clover.com/)===-1;
       var have_a_session_token = $window.sessionStorage.token;
       config.headers = config.headers || {};
-      if (have_a_session_token && not_aws_request && !clover_request) {
+      if (have_a_session_token && not_aws_request && not_clover_request) {
         //config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
         config.headers.Authorization  = $window.sessionStorage.token;
         config.headers['X-API-EMAIL'] = $window.sessionStorage.email;
-      } else if (clover_request) {
+      } else if (!not_clover_request) {
         config.headers.Authorization = "9234cf00-3b4d-6863-ed9d-d2d2310fff69";
       }
       return config;
