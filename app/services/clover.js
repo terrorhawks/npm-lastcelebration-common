2fd87aaf7c267796058e49dc3e87a5871494d10a
David Clarke <terrorhawks@gmail.com>
2015-03-17 12:38:16 +0000

    DRY for clover API

--- app/services/clover.js
+++ app/services/clover.js
@@ -1,32 +1,114 @@
 angular.module('common.services')
 
-.factory('Clover', ['$q', '$http', 'domainName', function($q, $http, domainName) {
+.constant('ecommerceInit', function($q, Clover) {
+
+        return function($q) {
+            var deferred = $q.defer();
+            Clover.setUp(deferred);
+            return deferred.promise;
+        };
+})
+
+.factory('Clover', ['$q', '$http', 'domainName', '$localstorage', function($q, $http, domainName, $localstorage) {
+
+    var storeInCache = function (key, items) {
+        $localstorage.setObject(key, items);
+    };
+
+    var getFromCache = function (key) {
+        return $localstorage.getObject(key);
+    };
+
+    var getItemsFromServer = function () {
+        var deferred = $q.defer();
+        $http.get(domainName + '/api/ecommerce/inventory')
+            .success(function(response) {
+                deferred.resolve(response);
+            })
+            .error(function (error, status) {
+                deferred.reject(error);
+            });
+        return deferred.promise;
+    };
+
+    var getConfigFromServer = function () {
+        var deferred = $q.defer();
+        $http.get(domainName + '/api/ecommerce/config')
+            .success(function(response) {
+                deferred.resolve(response);
+            })
+            .error(function (error, status) {
+                deferred.reject(error);
+            });
+        return deferred.promise;
+    };
 
     return {
 
-        items: function () {
+        setUp: function (deferred) {
+          $q.all([config(), items()]).then(function() {
+            deferred.resolve();
+          });
+        },
+
+        itemsForCategory: function (category) {
             var deferred = $q.defer();
-            $http.get(domainName + '/api/ecommerce/inventory')
-                .success(function(response) {
-                    deferred.resolve(response);
-                })
-                .error(function (error, status) {
-                    deferred.reject(error);
-                });
+            items().then(function (items) {
+                if (items) {
+                    deferred.resolve(items[category]);
+                }
+            }, function (e) {
+                deferred.reject(e);
+            });
             return deferred.promise;
         },
 
-        config: function (){
+        itemsForSubCategory: function (category, subCategory) {
             var deferred = $q.defer();
-            $http.get(domainName + '/api/ecommerce/config')
-                .success(function(response) {
-                    deferred.resolve(response);
-                })
-                .error(function (error, status) {
-                    deferred.reject(error);
-                });
+            itemsForCategory(category).then(function (items_for_category) {
+                if (items_for_category) {
+                    deferred.resolve(items_for_category[subCategory]);
+                }
+            }, function (e) {
+                deferred.reject(e);
+            });
             return deferred.promise;
         },
+
+        items: function () {
+            var deferred = $q.defer();
+            var key = 'ecommerce_items';
+            var items_from_cached = getFromCache(key);
+            if (items_from_cached===undefined) {
+               getItemsFromServer().then(function (items) {
+                    storeInCache(key, items);
+                    deferred.resolve(items);
+               }, function (e) {
+                    deferred.reject(e);
+               });
+            } else {
+                deferred.resolve(items_from_cached);
+            }
+            return deferred.promise;
+        },
+
+        config: function () {
+            var deferred = $q.defer();
+            var key = 'ecommerce_config';
+            var config_from_cached = getFromCache(key);
+            if (config_from_cached===undefined) {
+               getConfigFromServer().then(function (ecommerce_config) {
+                    storeInCache(key, ecommerce_config);
+                    deferred.resolve(ecommerce_config);
+               }, function (e) {
+                    deferred.reject(e);
+               });
+            } else {
+                deferred.resolve(config_from_cached);
+            }
+            return deferred.promise;
+        },
+
         lastOrder: function (id){
             var deferred = $q.defer();
             $http.get(domainName + '/api/ecommerce/orders/' + id)
