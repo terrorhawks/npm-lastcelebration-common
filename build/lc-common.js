0ec9836f6578d30b6aef8e087973902d13ccf2dd
David Clarke <terrorhawks@gmail.com>
2015-05-20 15:59:34 +0100

    Add cache expiry support

--- build/lc-common.js
+++ build/lc-common.js
@@ -90,6 +90,21 @@ angular.module('common.services')
 angular.module('common.services')
 
 .factory('$localstorage', ['$window', function($window) {
+  
+  var getObjectFromStorage = function(key) {
+      var cached_object = $window.localStorage[key];
+      if (cached_object) {
+        try {
+          return JSON.parse(cached_object);
+        } catch (exception) {
+          console.log(exception);
+          return undefined;
+        }
+      } else {
+        return undefined;
+      }
+  };
+
   return {
     
     set: function(key, value) {
@@ -104,8 +119,13 @@ angular.module('common.services')
       return $window.localStorage[key] || defaultValue;
     },
 
-    setObject: function(key, value) {
+    setObject: function(key, value, cacheTime) {
       if (value) {
+        if (cacheTime && cacheTime>0) {
+            var timeToExpire = new Date(Date.now() + cacheTime).getTime();
+            var cache_time_key = key + ".cacheTime";
+            $window.localStorage[cache_time_key] = timeToExpire;
+        }
         $window.localStorage[key] = JSON.stringify(value);
       } else {
         $window.localStorage.removeItem(key);
@@ -116,18 +136,21 @@ angular.module('common.services')
       $window.localStorage.clear();
     },
 
-    getObject: function(key) {
-      var cached_object = $window.localStorage[key];
-      if (cached_object) {
-        try {
-          return JSON.parse(cached_object);
-        } catch (exception) {
-          console.log(exception);
-          return undefined;
+    getObject: function(key, checkCacheExpiry) {
+      if (checkCacheExpiry) {
+        var cache_time_key = key + ".cacheTime";
+        var time_to_expire = $window.localStorage[cache_time_key];
+        var timenow = Date.now();
+        if (timenow <= time_to_expire) {
+          getObjectFromStorage(key);
+        }  else {
+          console.log("cache expired for key", key);
+          $window.localStorage.removeItem(key);
         }
       } else {
-        return undefined;
+        getObjectFromStorage(key);
       }
+      
     }
   };
 
