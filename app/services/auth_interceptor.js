cb475577d91d1d487b42ef78e2a45bab19c25e04
David Clarke <terrorhawks@gmail.com>
2016-01-26 11:41:04 +0000

    Ignore html requests, not authenticated

--- app/services/auth_interceptor.js
+++ app/services/auth_interceptor.js
@@ -9,27 +9,35 @@ angular.module('common.services')
   return {
     request: function (config) {
       
-      console.log("request interceptor", config);
+      var suffix = '.html';
+      var endsWithHTML = config.url.indexOf(suffix, config.url.length - suffix.length) !== -1;
 
-      var is_a_request_to_original_domain = config.url.search(domainName)!==-1;
-      var have_a_session_token = $localstorage.getObject(CACHE_TOKEN);
-      // var have_a_session_token = $window.sessionStorage.token;
-      config.headers = config.headers || {};
-      if (have_a_session_token && is_a_request_to_original_domain) {
-        // NOT REQUIRED config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
-        // config.headers.Authorization  = $window.sessionStorage.token;
-        // config.headers['X-API-EMAIL'] = $window.sessionStorage.email;
-        config.headers.Authorization  = have_a_session_token;
-        config.headers['X-API-EMAIL'] = $localstorage.getObject(CACHE_EMAIL);
-      }
-      if (companyUUID) {
-          // mobile apps use pre-configured companyUUID
-          config.headers['X-COMPANY-UUID'] = companyUUID;
-      } else {
-          // dashboard uses companyUUID from authenticated user
-           config.headers['X-COMPANY-UUID'] = $localstorage.getObject(CACHE_COMPANY_UUID);
-          // config.headers['X-COMPANY-UUID'] = $window.sessionStorage.companyUUID;
-      }
+      if (!endsWithHTML) {
+        
+        console.log("add tokens for request ", config.url);
+
+        var is_a_request_to_original_domain = config.url.search(domainName)!==-1;
+        var have_a_session_token = $localstorage.getObject(CACHE_TOKEN);
+        // var have_a_session_token = $window.sessionStorage.token;
+        config.headers = config.headers || {};
+        if (have_a_session_token && is_a_request_to_original_domain) {
+          // NOT REQUIRED config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+          // config.headers.Authorization  = $window.sessionStorage.token;
+          // config.headers['X-API-EMAIL'] = $window.sessionStorage.email;
+          config.headers.Authorization  = have_a_session_token;
+          config.headers['X-API-EMAIL'] = $localstorage.getObject(CACHE_EMAIL);
+        }
+        if (companyUUID) {
+            // mobile apps use pre-configured companyUUID
+            config.headers['X-COMPANY-UUID'] = companyUUID;
+        } else {
+            // dashboard uses companyUUID from authenticated user
+             config.headers['X-COMPANY-UUID'] = $localstorage.getObject(CACHE_COMPANY_UUID);
+            // config.headers['X-COMPANY-UUID'] = $window.sessionStorage.companyUUID;
+        }
+    } else {
+      console.log("do not add tokens for request " , config.url);
+    } 
 
       return config;
     },
