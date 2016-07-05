2a1e6bc8e6c4abbb5a746f3eceea7459b0689753
David Clarke <terrorhawks@gmail.com>
2016-07-05 16:21:18 +0100

    only one menu instance for get/delete/create/update

--- build/lc-common.js
+++ build/lc-common.js
@@ -694,40 +694,34 @@ angular.module('common.resources')
     var transformer = function(data, header) {
       console.log("transformer for menu", data, header);
        //Getting string data in response
-          var jsonData = angular.fromJson(data);
-          var menus = [];
-          var menu, category, product, optionGroup;
+          
+          var category, product, optionGroup;
 
-          angular.forEach(jsonData, function(menu) {
-            // menu = new Menu(menu);
-            if (menu.categories) {
-              var categories = [];
-              angular.forEach(menu.categories, function (category) {
-                category = new Category(category);
-                if (category.products) {
-                  var products = [];
-                  angular.forEach(category.products, function (product) {
-                    product = new Product(product);
-                    if (product.optionGroups) {
-                      var optionGroups = [];
-                      angular.forEach(product.optionGroups, function (optionGroup) {
-                        optionGroup = new ProductOptionGroup(optionGroup);
-                        optionGroups.push(optionGroup);
-                      });
-                      product.optionGroups = optionGroups;
-                    }
-                    products.push(product);
-                  });
-                  category.products = products;
-                }
-                categories.push(category);
-              });
-              menu.categories = categories;
-            }
-            menus.push(menu);
-          });
-          console.log("Menus Added", menus);
-          return menus;
+          if (data.categories) {
+            var categories = [];
+            angular.forEach(data.categories, function (category) {
+              category = new Category(category);
+              if (category.products) {
+                var products = [];
+                angular.forEach(category.products, function (product) {
+                  product = new Product(product);
+                  if (product.optionGroups) {
+                    var optionGroups = [];
+                    angular.forEach(product.optionGroups, function (optionGroup) {
+                      optionGroup = new ProductOptionGroup(optionGroup);
+                      optionGroups.push(optionGroup);
+                    });
+                    product.optionGroups = optionGroups;
+                  }
+                  products.push(product);
+                });
+                category.products = products;
+              }
+              categories.push(category);
+            });
+            data.categories = categories;
+          }
+          return data;
     };
 
     return $resource(domainName + '/api/dashboard/menus/:id', { id: '@id' }, {
@@ -743,8 +737,7 @@ angular.module('common.resources')
     },
     
     query: {
-      isArray: true,
-      transformResponse: transformer
+      isArray: true
     },
 
     get: {
