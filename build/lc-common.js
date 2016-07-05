35ef1ad56aa58ffaaa42a0f8cc656789e6d74530
David Clarke <terrorhawks@gmail.com>
2016-07-05 20:39:32 +0100

    Add transformer for category

--- build/lc-common.js
+++ build/lc-common.js
@@ -655,20 +655,55 @@ angular.module('common.resources')
 }]);
 angular.module('common.resources')
 
-.factory('Category', ['$resource', 'domainName', function ($resource, domainName) {
+.factory('Category', ['$resource', 'domainName','Product', 'ProductOptionGroup', function ($resource, domainName, Product, ProductOptionGroup) {
+
+   var transformer = function(data, header) {
+        var category, product, optionGroup;
+
+        category = angular.fromJson(data);
+
+        if (category.products) {
+          var products = [];
+          angular.forEach(category.products, function (product) {
+            product = new Product(product);
+            if (product.optionGroups) {
+              var optionGroups = [];
+              angular.forEach(product.optionGroups, function (optionGroup) {
+                optionGroup = new ProductOptionGroup(optionGroup);
+                optionGroups.push(optionGroup);
+              });
+              product.optionGroups = optionGroups;
+            }
+            products.push(product);
+          });
+          category.products = products;
+        }
+        return category;
+    };
+
 
   return  $resource(domainName + '/api/dashboard/categories/:id', { id: '@id' }, {
 
     create: {
-      method: 'POST'
+      method: 'POST',
+      transformResponse: transformer
     },
 
     update: {
-      method: 'PUT'
+      method: 'PUT',
+      transformResponse: transformer
     },
     
     query: {
       isArray: true
+    },
+
+    get: {
+      transformResponse: transformer
+    },
+
+    delete: {
+      transformResponse: transformer
     }
 
   });
