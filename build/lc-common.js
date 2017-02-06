dd8b6c6e750188c722097bc0711d21b69026bfa8
David Clarke <terrorhawks@gmail.com>
2017-02-06 11:01:18 +0000

    Add appVersion to cache key to avoid conflicts when deploying new releases

--- build/lc-common.js
+++ build/lc-common.js
@@ -400,10 +400,10 @@ angular.module('common.services')
  }]);
 angular.module('common.services')
 
-.factory('$hyperfoodstorage', ['$window', function($window) {
+.factory('$hyperfoodstorage', ['$window', 'appVersion', function($window, appVersion) {
   
   var getObjectFromStorage = function(key) {
-      var cached_object = $window.localStorage[key];
+      var cached_object = $window.localStorage[key + appVersion];
       if (cached_object) {
         try {
           return JSON.parse(cached_object);
@@ -420,28 +420,28 @@ angular.module('common.services')
     
     set: function(key, value) {
       if (value) {
-        $window.localStorage[key] = value;
+        $window.localStorage[key + appVersion] = value;
       } else {
-        $window.localStorage.removeItem(key);
+        $window.localStorage.removeItem(key + appVersion);
       }
     },
 
     get: function(key, defaultValue) {
-      return $window.localStorage[key] || defaultValue;
+      return $window.localStorage[key + appVersion] || defaultValue + appVersion;
     },
 
     setObject: function(key, value, cacheTime) {
       if (value) {
         if (cacheTime && cacheTime>0) {
             var timeToExpire = new Date(Date.now() + cacheTime).getTime();
-            var cacheTimeKey = key + ".cacheTime";
+            var cacheTimeKey = key + ".cacheTime" + appVersion;
             console.log("setting cache time to ", timeToExpire);
             $window.localStorage[cacheTimeKey] = timeToExpire;
         }
-        console.log("storing object in cache with key", key);
-        $window.localStorage[key] = JSON.stringify(value);
+        console.log("storing object in cache with key", key + appVersion);
+        $window.localStorage[key + appVersion] = JSON.stringify(value);
       } else {
-        $window.localStorage.removeItem(key);
+        $window.localStorage.removeItem(key + appVersion);
       }
     },
 
@@ -452,21 +452,21 @@ angular.module('common.services')
 
     getObject: function(key, checkCacheExpiry) {
       if (checkCacheExpiry) {
-        var cacheTimeKey = key + ".cacheTime";
+        var cacheTimeKey = key + ".cacheTime" + appVersion;
         var timeToExpire = $window.localStorage[cacheTimeKey];
         var timenow = Date.now();
         if (timenow <= timeToExpire) {
-          console.log("not expired yet, get from cache with key", key);
-          return getObjectFromStorage(key);
+          console.log("not expired yet, get from cache with key", key + appVersion);
+          return getObjectFromStorage(key + appVersion);
         }  else {
-          console.log("cache expired for key", key);
+          console.log("cache expired for key", key + appVersion);
           //lets avoid this to allow stale items to still be used in an emergency!
-          //$window.localStorage.removeItem(key);
+          //$window.localStorage.removeItem(key + appVersion);
           return undefined;
         }
       } else {
-        console.log("get object without checking cache time with key", key);
-        return getObjectFromStorage(key);
+        console.log("get object without checking cache time with key", key + appVersion);
+        return getObjectFromStorage(key + appVersion);
       }
     }
   };
